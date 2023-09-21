61d2ea3c00da4a4dc85f99529cda4bcc17574f61
Waseem <wasim_ahmad2003@yahoo.com>
2023-09-21 16:41:04 +0500

    added color-helper to generate rgba, rgb, and hex colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Palette from "./components/Palette";
 import seedColors from "./utils/seedColors";
+import { generatePalette } from "./utils/colorHelpers";
 
 export default class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div>
         <Palette {...seedColors[4]} />
